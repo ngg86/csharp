162c32dace140c60e633520300e9e1bc7a161977
Nicholas Greenslade <ng.greenslade@gmail.com>
2017-06-02 09:17:11 +0200

    Add files via upload

--- ADOnet/DeleteForm.cs
+++ ADOnet/DeleteForm.cs
@@ -48,17 +48,37 @@ namespace ADOnet
 
         private void btnDel_Click(object sender, EventArgs e)
         {
-            DeleteProduct();
+            
+            Products product = new Products();
+            //invalid cast?
+            product.productID = Convert.ToInt32((Products)cbDelProduct.SelectedItem);
+            product.productName = cbDelProduct.SelectedText;
+            DeleteProduct(product);
+            
         }
 
-        public void DeleteProduct()
+        public static int DeleteProduct(Products product)
         {
-            string item = cbDelProduct.SelectedItem.ToString();
-
+            //aantal gwiste rijen
+            int i = 0;
+            
+                       
             SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-698P2MHO\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
-            //SqlCommand cmd = new SqlCommand("DELETE FROM Products WHERE ProductName = item", con);
+            SqlCommand cmd = new SqlCommand("DELETE FROM Products WHERE ProductID = @productID", con);
+            SqlCommand scmd = new SqlCommand("SELECT COUNT ProductID FROM [OrderDetails] WHERE ProductID = @ProductID", con);
+            cmd.Parameters.AddWithValue("@productID", product.productID);
+            scmd.Parameters.AddWithValue("@productID", product.productID);
+            con.Open();
+            scmd.ExecuteScalar();
+            if (product.productID == 0)
+            {                
+                cmd.ExecuteScalar();
+                i++;
+            }
+            MessageBox.Show(i.ToString());
 
 
+            return i;
         }
     }
 }
---
+++ NorthwindClasses/Products.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindClasses
+{
+    public class Products
+    {
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public int? supplierID { get; set; }
+        public int? categorieID { get; set; }
+        public string quantityPerUnit { get; set; }
+        public decimal unitPrice { get; set; }
+        public int? unitInStock { get; set; }
+        public int? unitsOnOrder { get; set; }
+        public int? reorderLevel { get; set; }
+        public bool discontinued { get; set; }
+    }
+
+    public class Category
+    {
+        public int? categoryID { get; set; }
+        public string categoryName { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Customers
+    {
+        public int? customerID { get; set; }
+        public string companyName { get; set; }
+        public string contactName { get; set; }
+        public string contactTitle { get; set; }
+        public string Adres { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string postalCode { get; set; }
+        public string Country { get; set; }
+        public int Phone { get; set; }
+        public int? Fax { get; set; }
+    }
+
+    public class CustomerCustomerDemo
+    {
+        public string customerTypeID;
+        public string customerID;
+    }
+
+    public class CustomerDemographics : CustomerCustomerDemo
+    {
+
+    }
+
+    public class Employees
+    {
+        public int? employeeID { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string Title { get; set; }
+        public string titleOfCourtesy { get; set; }
+        public double birthDate { get; set; }
+        public double hireDate { get; set; }
+        public string Adres { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string postalCode { get; set; }
+        public string Country { get; set; }
+        public int homePhonepublic { get; set; }
+        public int Extension { get; set; }
+        public string Notes { get; set; }
+        public int? reportsTo { get; set; }
+
+
+    }
+
+    public class EmployeeTerritories
+    {
+        public int? employeeID { get; set; }
+        public string territoryID { get; set; }
+    }
+
+    public class Orders
+    {
+        public int? orderID { get; set; }
+        public string customerID { get; set; }
+        public int? employeeID { get; set; }
+        public DateTime orderDate { get; set; }
+        public DateTime requiredDate { get; set; }
+        public DateTime shippedDate { get; set; }
+        public int? shipVia { get; set; }
+        public decimal Freight { get; set; }
+        //nog niet af
+    }
+}
